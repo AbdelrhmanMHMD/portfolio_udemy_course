0b5b6c8b53ab43e37c601df627e5532fc33c946f
AbdelrhmanMHMD <am600680@gmail.com>
2024-10-24 16:07:51 +0300

    .

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import "./topbar.scss";
 const Topbar = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
 	return (
-		<div className="topbar">
+		<div className={"topbar " + (menuOpen ? "active" : "")}>
 			<div className="wrapper">
 				<div className="left">
-					<a href='#intro' className="logo">Genius.</a>
+					<a href="#intro" className="logo">
+						genius.
+					</a>
+					<div className="itemContainer">
+						<i className="fa-solid fa-user"></i>
+						<span>+201029230204</span>
+					</div>
+					<div className="itemContainer">
+						<i className="fa-solid fa-envelope"></i>
+						<span>am600680@gmail.com</span>
+					</div>
 				</div>
 				<div className="right">
-					right
+					<div
+						className="hamburger"
+						onClick={() => setMenuOpen(!menuOpen)}
+					>
+						<span className="line1"></span>
+						<span className="line2"></span>
+						<span className="line3"></span>
+					</div>
 				</div>
 			</div>
 		</div>
