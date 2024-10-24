8051f89ff3788cdbe1398415f477cdfba954180b
AbdelrhmanMHMD <am600680@gmail.com>
2024-10-24 14:55:32 +0300

    f

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,6 @@
+import './intro.scss'
+const Intro = () => {
+    return ( <div className="intro" id='intro'></div> );
+}
+ 
+export default Intro;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import "./topbar.scss";
+const Topbar = () => {
+	return (
+		<div className="topbar">
+			<div className="wrapper">
+				<div className="left">
+					<a href='#intro' className="logo">Genius.</a>
+				</div>
+				<div className="right">
+					right
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Topbar;
